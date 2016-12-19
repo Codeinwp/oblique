95f65512c69203ce3d916938d8a4167dff083610
Rodica-Elena Andronache <rodica.andronache@vertistudio.com>
2016-12-19 17:33:23 +0200

    Revert "Update files location for grunt"
    This reverts commit b644ea3adeea98ffe27dd220e98f35f174ec0250.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,16 +7,6 @@ module.exports = function( grunt ) {
         config = require( 'grunt-theme-fleet' );
     config = config();
     config.files.php.push( '!inc/admin/**/*.php' );
-    config.files.php.push( '!class-tgm-plugin-activation.php' );
     config.files.js.push( '!inc/admin/**/*.js' );
-
-    config.files.js.push( '!js/imagesloaded.pkgd.min.js' );
-    config.files.js.push( '!js/imagesloaded.pkgd.js' );
-    config.files.js.push( '!js/jquery.fitvids.js' );
-    config.files.js.push( '!js/jquery.slicknav' );
-    config.files.js.push( '!js/main.js' );
-    config.files.js.push( '!js/masonry-init.js' );
-    config.files.js.push( '!js/html5shiv.js' );
-    config.files.js.push( '!js/parallax.js' );
     loader( grunt, config ).init();
 };
